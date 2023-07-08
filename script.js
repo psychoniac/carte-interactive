7fa901dfe3108ee898aaacce2083acac14727b39
psychoniac <jlnko@hotmail.fr>
2023-07-09 01:51:01 +0200

    premier commit initialisation de la librairie pour la carte et ajout du style

---
+++ script.js
@@ -0,0 +1,6 @@
+let map = L.map('map').setView([46.136, -1.151], 15);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
